4904f0908daa7f9d0e435cef21e869bcbce56e6f
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-06-07 11:47:08 +0300

    Add about component

--- client/handtime/src/app/app-routing.module.ts
+++ client/handtime/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './feature/pages/home/home.component';
+import { AboutComponent } from './feature/pages/about/about.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
--- client/handtime/src/app/feature/pages/page.module.ts
+++ client/handtime/src/app/feature/pages/page.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AboutComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HomeComponent],
+  exports: [HomeComponent, AboutComponent],
 })
 export class PageModule {}
