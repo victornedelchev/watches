5fa7fd57276a0f868739ab3ad5c291eb8d8d1d3c
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-06-07 11:25:50 +0300

    Add home component

--- client/handtime/src/app/app.module.ts
+++ client/handtime/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { FeatureModule } from './feature/feature.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, FeatureModule],
   providers: [],
   bootstrap: [AppComponent],
 })
