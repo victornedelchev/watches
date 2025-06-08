2a4ab1d40c00216d4661ba09ca3c966f8889cb3a
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-06-08 09:16:36 +0300

    Add title to testimanial page

--- client/handtime/src/app/feature/pages/testimonial/testimonial.component.ts
+++ client/handtime/src/app/feature/pages/testimonial/testimonial.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-testimonial',
   templateUrl: './testimonial.component.html',
-  styleUrls: ['./testimonial.component.css']
+  styleUrls: ['./testimonial.component.css'],
 })
-export class TestimonialComponent {
+export class TestimonialComponent implements OnInit {
+  constructor(private titleService: Title) {}
 
+  ngOnInit(): void {
+    this.titleService.setTitle('Testimonial Page');
+  }
 }
