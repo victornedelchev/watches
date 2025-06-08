12a346e3a1312261283cbd9689968ec2b89ffd5b
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-06-08 17:56:49 +0300

    Add route guard
    For add watch and profile paths

--- client/handtime/src/app/auth/auth-routing.module.ts
+++ client/handtime/src/app/auth/auth-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from '../core/guards/auth.guards';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'profile', canActivate: [AuthGuard], component: ProfileComponent },
 ];
 
 @NgModule({
--- client/handtime/src/app/feature/watches/watches-routing-module.ts
+++ client/handtime/src/app/feature/watches/watches-routing-module.ts
@@ -3,10 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { WatchListComponent } from './watch-list/watch-list.component';
 import { WatchDetailsComponent } from './watch-details/watch-details.component';
 import { AddWatchComponent } from './add-watch/add-watch.component';
+import { AuthGuard } from 'src/app/core/guards/auth.guards';
 
 const routes: Routes = [
   { path: 'watches', component: WatchListComponent },
-  { path: 'watches/add', component: AddWatchComponent },
+  {
+    path: 'watches/add',
+    canActivate: [AuthGuard],
+    component: AddWatchComponent,
+  },
   { path: 'watches/:_id', component: WatchDetailsComponent },
 ];
 
