3d8b907779251d30d9b9e797e3dc69ec6f41d37e
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-06-08 15:27:31 +0300

    Add logout fuctionality

---
+++ client/handtime/src/app/core/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { StorageService } from './storage.sevice';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  isLogged: boolean = false;
+
+  constructor(private storage: StorageService) {
+    this.isLogged = this.storage.getItem('isLogged') ?? false;
+  }
+
+  login(): void {
+    this.isLogged = true;
+    this.storage.setItem('isLogged', true);
+  }
+
+  logOut(): void {
+    this.isLogged = false;
+    this.storage.setItem('isLogged', false);
+  }
+}
