372824a344c26104663caf90518d5802c475e940
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-06-09 16:53:07 +0300

    Add title to profile page

--- client/handtime/src/app/auth/profile/profile.component.ts
+++ client/handtime/src/app/auth/profile/profile.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  constructor(private titleService: Title) {}
 
+  ngOnInit(): void {
+    this.titleService.setTitle('Profile Page');
+  }
 }
