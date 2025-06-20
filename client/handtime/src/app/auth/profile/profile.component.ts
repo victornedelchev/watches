7f09bca9f055d1ac13ba6cc7996d5ccb9c5d0b60
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-06-20 18:46:58 +0300

    Add details for user

--- client/handtime/src/app/auth/profile/profile.component.ts
+++ client/handtime/src/app/auth/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { IUser } from 'src/app/core/interfaces/user';
+import { UserService } from 'src/app/core/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +9,11 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  constructor(private titleService: Title) {}
+  get currentUser(): IUser {
+    return this.userService.currentUser;
+  }
+
+  constructor(private titleService: Title, private userService: UserService) {}
 
   ngOnInit(): void {
     this.titleService.setTitle('Profile Page');
