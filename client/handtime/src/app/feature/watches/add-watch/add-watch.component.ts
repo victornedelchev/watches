5ffc5c2b8cd765c0f6b96b09ec353471f322e0fd
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-06-21 14:36:42 +0300

    Add form validation functionality
    With template-driven way

--- client/handtime/src/app/feature/watches/add-watch/add-watch.component.ts
+++ client/handtime/src/app/feature/watches/add-watch/add-watch.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
+
+import {
+  faExclamationTriangle,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add-watch',
@@ -7,9 +12,17 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./add-watch.component.css'],
 })
 export class AddWatchComponent implements OnInit {
+  faExclamationTriangle = faExclamationTriangle;
+  errorMessage: string = '';
+
   constructor(private titleService: Title) {}
 
   ngOnInit(): void {
     this.titleService.setTitle('Add New Watch Page');
+  }
+
+  onAddWatch(addWatchForm: NgForm) {
+    this.errorMessage = '';
+    console.log(addWatchForm.value);
   }
 }
--- client/handtime/src/app/feature/watches/watches.module.ts
+++ client/handtime/src/app/feature/watches/watches.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { WatchListComponent } from './watch-list/watch-list.component';
 import { WatchesRoutingModule } from './watches-routing-module';
 import { WatchListItemComponent } from './watch-list-item/watch-list-item.component';
-import { RouterModule } from '@angular/router';
 import { WatchDetailsComponent } from './watch-details/watch-details.component';
 import { AddWatchComponent } from './add-watch/add-watch.component';
-import { SharedModule } from "../../shared/shared.module";
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [WatchListComponent, WatchListItemComponent, WatchDetailsComponent, AddWatchComponent],
-  imports: [CommonModule, RouterModule, SharedModule],
+  declarations: [
+    WatchListComponent,
+    WatchListItemComponent,
+    WatchDetailsComponent,
+    AddWatchComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    FontAwesomeModule,
+    FormsModule,
+  ],
   exports: [WatchListComponent, WatchesRoutingModule],
 })
 export class WatchesModule {}
