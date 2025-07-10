c36a84858e0440f3a1a2629db259194a0e780bfc
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-07-10 17:26:14 +0300

    Add toogle text functionality

---
+++ client/handtime/src/app/constants/description.ts
@@ -0,0 +1,3 @@
+const description: string = 'This is an application for lovers and admirers of wristwatches. Here you can browse watches added by other users, as well as add comments about them. After registering, you have the opportunity to add an unlimited number of watches that you like.'
+
+export default description;
--- client/handtime/src/app/feature/pages/about/about.component.ts
+++ client/handtime/src/app/feature/pages/about/about.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
+import description from '../../../constants/description';
+
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-  constructor(private titleService: Title) {}
+  readMore: boolean = true;
+  description = description;
+  constructor(private titleService: Title) { }
 
   ngOnInit(): void {
     this.titleService.setTitle('About Page');
+  }
+
+  toggleReadMore(): void {
+    this.readMore = !this.readMore;
   }
 }
