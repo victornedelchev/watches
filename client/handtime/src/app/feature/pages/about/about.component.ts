519d8aa1ab0dd70b78b3b7c6fe64300fc9332365
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-07-11 15:54:00 +0300

    Add Google maps

--- client/handtime/src/app/feature/pages/about/about.component.ts
+++ client/handtime/src/app/feature/pages/about/about.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 import description from '../../../constants/description';
+import { Loader } from '@googlemaps/js-api-loader';
 
-
+declare const google: any;
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
+
 export class AboutComponent implements OnInit {
   readMore: boolean = true;
   description = description;
@@ -16,6 +18,23 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {
     this.titleService.setTitle('About Page');
+
+    let loader = new Loader({
+      apiKey: 'AIzaSyBVi7HNr2M81GiZWLOSFrVu43TCkN9MGbI',
+    });
+
+    loader.load().then(() => {
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 43.345082, lng: 27.195437 },
+        zoom: 17,
+      });
+
+      new google.maps.Marker({
+        position: { lat: 43.345082, lng: 27.195437 },
+        map: map,
+        title: 'Checkpoint'
+      })
+    })
   }
 
   toggleReadMore(): void {
